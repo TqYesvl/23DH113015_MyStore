622f67a72a2290ddf0cb79caa9fe3b1b1ab21312
SV <anhxgpt2@gmail.com>
2024-10-14 11:13:35 +0700

    video 2

--- 23DH113015_MyStore/Controllers/HomeController.cs
+++ 23DH113015_MyStore/Controllers/HomeController.cs
@@ -8,21 +8,10 @@ namespace _23DH113015_MyStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
+        public ActionResult TrangChu()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
